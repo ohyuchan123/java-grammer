8fa56ba3a398bec1cd5b39fb58cd08e26b72571f
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-11-24 01:23:10 +0900

    문자열 출력

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         //psvm (public static void main(String[] args)의 단축키
+
+        System.out.println("Hello World!!");
+        //sout : System.out.println의 단축키
     }
 }
---
+++ src/String_Output/Introduce_yf.java
@@ -0,0 +1,9 @@
+package String_Output;
+
+public class Introduce_yf {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요! 반값습니다.");
+        System.out.println("현재 백엔드(spring boot)를 공부하고 있는 오유찬 입니다.");
+        System.out.println("잘 부탁드립니다.");
+    }
+}
