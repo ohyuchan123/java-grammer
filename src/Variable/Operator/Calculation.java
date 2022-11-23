db7a9b7f1ffd6376ae7f0fd3dcea0b0027906107
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-11-24 01:50:08 +0900

    계산

---
+++ src/Variable/Operator/Calculation.java
@@ -0,0 +1,11 @@
+package Variable.Operator;
+
+public class Calculation {
+    public static void main(String[] args) {
+        double score = 1.0+2.0*3.0;
+
+        System.out.println("점수 출력 = "+score);
+        score = score/2.0;
+        System.out.println("점수 출력 = "+score);
+    }
+}
