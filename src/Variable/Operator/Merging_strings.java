e00f755a99ac560945f2294c8ff5ea0ca05a6061
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-11-24 01:50:17 +0900

    문자열 병합

---
+++ src/Variable/Operator/Merging_strings.java
@@ -0,0 +1,10 @@
+package Variable.Operator;
+
+public class Merging_strings {
+    public static void main(String[] args) {
+        String text = "hello" + "world";
+        text = text+"number"+5;
+
+        System.out.println(text);
+    }
+}
