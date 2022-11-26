7ea5a20d533080a115978b6ab837a02c441fc228
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-11-27 00:18:13 +0900

    타입 불일치

---
+++ src/TypeAndTypeChange/TypeMismatch.java
@@ -0,0 +1,12 @@
+package TypeAndTypeChange;
+
+public class TypeMismatch {
+    /*값을 대입시 타입이 다르면 프로그램이 동작하지 않습니다.*/
+
+    public static void main(String[] args) {
+
+            /*String five = 5; -> */int five = 5;
+        System.out.println(five);
+
+    }
+}
