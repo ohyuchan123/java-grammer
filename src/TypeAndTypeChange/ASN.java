de085a91e452bed934634428818613587158a43e
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-11-27 00:21:33 +0900

    문자열을 숫자로

---
+++ src/TypeAndTypeChange/ASN.java
@@ -0,0 +1,12 @@
+package TypeAndTypeChange;
+
+public class ASN {
+    //문자열을 숫자로
+    public static void main(String[] args) {
+        int seven = Integer.parseInt("7");
+        double pi = Double.parseDouble("3.14");
+
+        System.out.println("seven = "+seven);
+        System.out.println("pi = "+pi);
+    }
+}
