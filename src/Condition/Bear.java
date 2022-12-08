c28371adbf79107996ca2a03b3775634132f8cd1
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2022-12-08 13:43:49 +0900

    조건문(인증 : 비교연산자)

--- src/Condition/Bear.java
+++ src/Condition/Bear.java
@@ -8,6 +8,8 @@ public class Bear {
         //18세 이상인 경우 메세지를 출력!
         if(age>=18){
             System.out.println("주문이 완료되었습니다!");
+        }else{
+            System.out.println("주문을 할 수 없는 나이입니다!");
         }
     }
 
